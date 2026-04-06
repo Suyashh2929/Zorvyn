a83f53ce23b12212cfc92e5ac30f1d86954eb1b0
Suyash Sharma <130591034+Suyashh2929@users.noreply.github.com>
2026-04-06 19:07:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
